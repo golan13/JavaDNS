31b7a3f7b64ba4d41c95f24494342a8ad9f78556
Golan Cohen <golan13@gmail.com>
2021-01-03 22:11:30 +0200

    Added layout

--- src/Lab.java
+++ src/Lab.java
@@ -1,5 +1,24 @@
+import java.net.DatagramSocket;
+import java.net.SocketException;
+
 public class Lab {
     public static void main(String[] args) {
-        System.out.println("hello");
+        /*
+        1. Listen on UDP port 5300
+        2. Send packet to random root server
+        3. while (response code is NOERROR) && (ANSWER in record is 0) && (AUTHORITY > 0) {
+            Send the query to the first name server in the AUTHORITY section
+           }
+        4. Send the final response to the client
+         */
+        try{
+            DatagramSocket s = new DatagramSocket(5300);
+        } catch (SocketException e) {
+            System.out.println("There was a problem creating the socket.");
+            System.out.println("The program will now exit");
+            return;
+        }
+
+
     }
 }
