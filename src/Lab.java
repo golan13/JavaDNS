141472a82832aa46203ebde9464bfed3d15e502d
Golan Cohen <golan13@gmail.com>
2021-01-03 22:32:41 +0200

    Added random root server

--- src/Lab.java
+++ src/Lab.java
@@ -1,8 +1,11 @@
+import java.io.IOException;
 import java.net.DatagramSocket;
+import java.net.InetAddress;
 import java.net.SocketException;
+import java.util.Random;
 
 public class Lab {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException{
         /*
         1. Listen on UDP port 5300
         2. Send packet to random root server
@@ -18,7 +21,11 @@ public class Lab {
             System.out.println("The program will now exit");
             return;
         }
+        System.out.println(getRoot());
+    }
 
-
+    public static InetAddress getRoot() throws IOException {
+        char r =(char)(int)(Math.random() * 13 + 97);
+        return InetAddress.getByName(r + ".root-servers.net");
     }
 }
